4133606c8637a1f7f925bfd4bfb2e13e0c52e218
Gabriel Stawny <gastawny@gmail.com>
2023-02-03 18:54:42 -0300

    Initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.scss'
+
+const Header = () => {
+    return (
+        <div>
+            {/* <img src={foto} /> */}
+        </div>
+    )
+}
+
+export default Header
