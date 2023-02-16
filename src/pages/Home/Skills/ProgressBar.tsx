3ea19d0422c997655dcbf323541a7c3102ec31e9
Gabriel Stawny <gastawny@gmail.com>
2023-02-15 21:25:17 -0300

    Refactoring the ProgressBar component

--- src/pages/Home/Skills/ProgressBar.tsx
+++ src/pages/Home/Skills/ProgressBar.tsx
@@ -1,4 +1,5 @@
-import { ReactElement } from 'react'
+import useWidth from 'hooks/useWidth'
+import { ReactElement, useEffect, useState } from 'react'
 import styled, { keyframes } from 'styled-components'
 
 interface ProgressBarProps {
@@ -8,32 +9,29 @@ interface ProgressBarProps {
   fontSize: number
 }
 
-interface StyledProps {
-  progress?: number
-  fontSize?: string
-  widthConst?: number
-}
-
 const ProgressBar = ({ progress, children, fontSize, name }: ProgressBarProps) => {
-  const width = window.innerWidth
-  let widthConst = 1
-  if (width >= 768 && width <= 1366) widthConst = 0.8
+  const width = useWidth()
+  const [widthConst, setWidthConst] = useState(1)
 
-  return widthConst ? (
+  useEffect(() => {
+    console.log(width)
+    if (width >= 1366) setWidthConst(1)
+    if (width <= 1366) setWidthConst(0.8)
+  }, [width])
+
+  return (
     <ProgressBarStyled>
-      <Percent widthConst={widthConst}>
+      <Percent>
         <Dot progress={progress}></Dot>
-        <CircleProgressContainer widthConst={widthConst}>
+        <CircleProgressContainer>
           <CircleProgress
             progress={progress}
-            widthConst={widthConst}
             cx={84 * widthConst}
             cy={84 * widthConst}
             r={84 * widthConst}
           ></CircleProgress>
           <CircleProgress
             progress={progress}
-            widthConst={widthConst}
             cx={84 * widthConst}
             cy={84 * widthConst}
             r={84 * widthConst}
@@ -41,12 +39,10 @@ const ProgressBar = ({ progress, children, fontSize, name }: ProgressBarProps) =
         </CircleProgressContainer>
         <Content>
           {children}
-          <LanguageName fontSize={(fontSize * widthConst).toString() + 'rem'}>{name}</LanguageName>
+          <LanguageName fontSize={fontSize}>{name}</LanguageName>
         </Content>
       </Percent>
     </ProgressBarStyled>
-  ) : (
-    <></>
   )
 }
 
@@ -57,10 +53,15 @@ const ProgressBarStyled = styled.div`
   align-items: center;
 `
 
-const Percent = styled.div<StyledProps>`
+const Percent = styled.div`
   position: relative;
-  width: ${({ widthConst }) => (widthConst ? 11.25 * widthConst : 11.25)}rem;
-  height: ${({ widthConst }) => (widthConst ? 11.25 * widthConst : 11.25)}rem;
+  width: 11.25rem;
+  height: 11.25rem;
+
+  @media (max-width: 1366px) {
+    width: 9rem;
+    height: 9rem;
+  }
 `
 
 const fadeIn = keyframes`
@@ -73,69 +74,74 @@ const fadeIn = keyframes`
     }
 `
 
-const animateDot = (props: StyledProps) => keyframes`
+const animateDot = (progress: number) => keyframes`
     0% {
         transform: rotate(0deg);
     }
 
     100% {
-        transform: rotate(calc(3.6deg * ${props.progress}));
+        transform: rotate(calc(3.6deg * ${progress}));
     }
 `
 
-const Dot = styled.div<StyledProps>`
+const Dot = styled.div<{ progress: number }>`
   position: absolute;
-  inset: ${({ widthConst }) => (widthConst ? 0.375 * widthConst : 0.375)}rem;
+  inset: 0.375rem;
   z-index: 10;
-  animation: ${(props) => animateDot(props)} 2s linear forwards;
+  animation: ${({ progress }) => animateDot(progress)} 2s linear forwards;
+
+  @media (max-width: 1366px) {
+    inset: 0.3rem;
+  }
 
   &:before {
     content: '';
     position: absolute;
-    top: ${({ widthConst }) => (widthConst ? -0.45 * widthConst : -0.45)}rem;
+    top: -0.45rem;
     left: 50%;
     transform: translateX(-50%);
-    width: ${({ widthConst }) => (widthConst ? 0.9 * widthConst : 0.9)}rem;
-    height: ${({ widthConst }) => (widthConst ? 0.9 * widthConst : 0.9)}rem;
+    width: 0.9rem;
+    height: 0.9rem;
     border-radius: 50%;
     background: var(--blue);
     box-shadow: 0 0 0.75rem var(--blue), 0 0 2.25rem var(--blue);
+
+    @media (max-width: 1366px) {
+      width: 0.72rem;
+      height: 0.72rem;
+      top: -0.36rem;
+    }
   }
 `
-/* eslint-disable */
-const CircleProgress = styled.circle<StyledProps>`
+
+const CircleProgress = styled.circle<{ progress: number }>`
   width: 100%;
   height: 100%;
   fill: transparent;
   stroke-width: 6;
   stroke: #191919;
   transform: translate(0.375rem, 0.375rem);
-  stroke-width: ${({ widthConst }) => (widthConst ? 6 * widthConst : 6)};
-  stroke: #191919;
-  transform: ${({ widthConst }) =>
-    widthConst
-      ? 'translate(' +
-        (0.375 * widthConst).toString() +
-        'rem' +
-        ',' +
-        (0.375 * widthConst).toString() +
-        'rem' +
-        ')'
-      : 'translate(0.375rem, 0.375rem)'};
+
+  @media (max-width: 1366px) {
+    stroke-width: 4.8;
+    transform: translate(0.3rem, 0.3rem);
+  }
 
   &:nth-child(2) {
     stroke: var(--blue);
-    stroke-dasharray: ${({ widthConst }) => (widthConst ? 528 * widthConst : 528)};
-    stroke-dashoffset: calc(
-      ${({ widthConst, progress }) =>
-        widthConst && progress ? 528 * widthConst - (528 * widthConst * progress) / 100 : 0}
-    );
+    stroke-dasharray: 528;
+    stroke-dashoffset: calc(528 - (528 * ${({ progress }) => progress}) / 100);
     opacity: 0;
     animation: ${fadeIn} 1s linear forwards;
     animation-delay: 2.2s;
+
+    @media (max-width: 1366px) {
+      stroke-dasharray: 422.4;
+      stroke-dashoffset: calc(422.4 - (422.4 * ${({ progress }) => progress}) / 100);
+    }
   }
 `
-/* eslint-enable */
+
 const Content = styled.div`
   position: absolute;
   inset: 0;
@@ -149,17 +155,26 @@ const Content = styled.div`
   animation-delay: 2.2s;
 `
 
-const LanguageName = styled.div<StyledProps>`
+const LanguageName = styled.div<{ fontSize: number }>`
   font-weight: 600;
-  font-size: ${({ fontSize }) => fontSize};
+  font-size: ${({ fontSize }) => fontSize}rem;
   text-align: center;
   margin-bottom: 5%;
+
+  @media (max-width: 1366px) {
+    font-size: ${({ fontSize }) => fontSize * 0.8}rem;
+  }
 `
 
-const CircleProgressContainer = styled.svg<StyledProps>`
+const CircleProgressContainer = styled.svg`
   transform: rotate(270deg);
   position: relative;
-  width: ${({ widthConst }) => (widthConst ? 11.25 * widthConst : 11.25)}rem;
-  height: ${({ widthConst }) => (widthConst ? 11.25 * widthConst : 11.25)}rem;
+  width: 11.25rem;
+  height: 11.25rem;
+
+  @media (max-width: 1366px) {
+    width: 9rem;
+    height: 9rem;
+  }
 `
 export default ProgressBar
