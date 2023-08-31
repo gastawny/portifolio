4c4e3bd5f56f99c02256573838791a97bacce4c0
gastawny <gastawny@gmail.com>
2023-08-30 21:44:08 -0300

    Create SVGs icons components

--- src/components/svgs/index.tsx
+++ src/components/svgs/index.tsx
@@ -7,6 +7,23 @@ import { svgHOC } from './svgHOC'
 import { Telegram } from './telegram'
 import { WhatsApp } from './whatsApp'
 import { LinkExternal } from './linkExternal'
+import { TypeScript } from './typescript'
+import { Java } from './java'
+import { JavaScript } from './javascript'
+import { MySQL } from './mysql'
+import { Nextjs } from './nextjs'
+import { Nodejs } from './nodejs'
+import { React } from './react'
+import { Sass } from './sass'
+import { StyledComponents } from './styledComponents'
+import { Tailwind } from './tailwind'
+import { Nestjs } from './nestjs'
+import { Golang } from './golang'
+import { Postgres } from './postgres'
+import { MongoDB } from './mongodb'
+import { Rust } from './rust'
+import { Prisma } from './prisma'
+import { Spring } from './spring'
 
 const DiscordWithSVG = svgHOC(Discord)
 const GitHubWithSVG = svgHOC(GitHub)
@@ -14,6 +31,23 @@ const LinkedinWithSVG = svgHOC(Linkedin)
 const WhatsAppWithSVG = svgHOC(WhatsApp)
 const TelegramWithSVG = svgHOC(Telegram)
 const LinkExternalWithSVG = svgHOC(LinkExternal)
+const TypeScriptWithSVG = svgHOC(TypeScript)
+const JavaWithSVG = svgHOC(Java)
+const JavaScriptWithSVG = svgHOC(JavaScript)
+const MySQLWithSVG = svgHOC(MySQL)
+const NextjsWithSVG = svgHOC(Nextjs)
+const NodejsWithSVG = svgHOC(Nodejs)
+const ReactWithSVG = svgHOC(React)
+const SassWithSVG = svgHOC(Sass)
+const StyledComponentsWithSVG = svgHOC(StyledComponents)
+const TailwindWithSVG = svgHOC(Tailwind)
+const NestjsWithSVG = svgHOC(Nestjs)
+const GolangWithSVG = svgHOC(Golang)
+const PostgresWithSVG = svgHOC(Postgres)
+const MongoDBWithSVG = svgHOC(MongoDB)
+const RustWithSVG = svgHOC(Rust)
+const PrismaWithSVG = svgHOC(Prisma)
+const SpringWithSVG = svgHOC(Spring)
 
 export {
   DiscordWithSVG as Discord,
@@ -22,4 +56,21 @@ export {
   WhatsAppWithSVG as WhatsApp,
   TelegramWithSVG as Telegram,
   LinkExternalWithSVG as LinkExternal,
+  TypeScriptWithSVG as TypeScript,
+  JavaWithSVG as Java,
+  JavaScriptWithSVG as JavaScript,
+  MySQLWithSVG as MySQL,
+  NextjsWithSVG as Nextjs,
+  NodejsWithSVG as Nodejs,
+  ReactWithSVG as React,
+  SassWithSVG as Sass,
+  StyledComponentsWithSVG as StyledComponents,
+  TailwindWithSVG as Tailwind,
+  NestjsWithSVG as Nestjs,
+  GolangWithSVG as Golang,
+  PostgresWithSVG as Postgres,
+  MongoDBWithSVG as MongoDB,
+  RustWithSVG as Rust,
+  PrismaWithSVG as Prisma,
+  SpringWithSVG as Spring,
 }
--- src/components/svgs/svgHOC/index.tsx
+++ src/components/svgs/svgHOC/index.tsx
@@ -1,51 +1,19 @@
 'use client'
 
-import { useThrottle } from '@/hooks/useThrottle'
+import { useWidth } from '@/hooks/useWidth'
 import { ISVG } from '@/interfaces/Svg'
-import { useEffect, useLayoutEffect, useState } from 'react'
 
 export function svgHOC(Component: any) {
   return function Wrapper({ responsiveSize, ...props }: ISVG) {
     'use client'
-    const [size, setSize] = useState(0)
-    const [constant, setConstant] = useState(0)
-
-    function updateConstant() {
-      setConstant(() => {
-        if (size > 1536) return 1
-        else if (size >= 500) return 0.8
-        else if (size > 0) return 0.5
-        else {
-          const widthLayoutType = window.innerWidth
-          if (widthLayoutType > 1536) return 1
-          else if (widthLayoutType >= 500) return 0.8
-          else return 0.5
-        }
-      })
-    }
-
-    const throttledSetSize = useThrottle(() => {
-      setSize(window.innerWidth)
-    }, 500)
-
-    useLayoutEffect(() => {
-      updateConstant()
-
-      window.addEventListener('resize', throttledSetSize)
-
-      return () => window.removeEventListener('resize', throttledSetSize)
-    }, [])
-
-    useEffect(() => {
-      updateConstant()
-    }, [size])
+    const { layout } = useWidth()
 
     const modifiedProps = {
       ...props,
       fontSize: responsiveSize
-        ? constant === 1
+        ? layout === '2xl'
           ? responsiveSize['2xl']
-          : constant === 0.8
+          : layout === 'xl'
           ? responsiveSize['xl']
           : responsiveSize['sm']
         : props.fontSize,
